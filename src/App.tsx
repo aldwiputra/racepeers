31ab541e6c79764a89570f7f112f95039ff7ac1b
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-06-08 13:50:18 +0700

    add register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { useState } from 'react';
 import { gameStateChanger } from './utils/gameStateChanger';
+import RegisterForm from './components/RegisterForm';
 
 export type GameState = 'Register' | 'Playing' | 'Finished';
 
 function App() {
   const [gameState, setGameState] = useState<GameState>('Register');
 
-  return (
-    <main className='font-inter'>
-      <h1 className='text-5xl font-semibold'>Game State: {gameState}</h1>
-      <button onClick={() => gameStateChanger(gameState, setGameState)}>Change State</button>
-    </main>
-  );
+  return <main className='font-inter'>{gameState === 'Register' && <RegisterForm />}</main>;
 }
 
 export default App;
