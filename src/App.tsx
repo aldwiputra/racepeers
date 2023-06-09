da0659926e7cec90af3f084f44f4358daeca09fd
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-06-09 09:37:48 +0700

    add scoreboard

--- src/App.tsx
+++ src/App.tsx
@@ -111,12 +111,29 @@ function App() {
                   }
                   playersTurnChanger(playersTurn, setPlayersTurn);
                 }}
-                className='px-6 py-3 font-semibold bg-sky-600 rounded-md mt-4 text-lg'>
+                className='px-6 py-3 font-semibold bg-sky-600 rounded-md mt-4 text-lg disabled:bg-zinc-500'>
                 {gameState === 'Playing' && 'Race Away!'}
                 {gameState === 'Finished' && 'Finished!'}
               </button>
             </div>
-            <pre>{JSON.stringify(scoreBoard)}</pre>
+            {scoreBoard.length > 0 && (
+              <div className=' text-center mt-8 px-8 py-4 bg-slate-800 rounded-md ring-2 ring-slate-700/75'>
+                <h5 className='text-center font-semibold text-2xl'>Race Result</h5>
+                <div className='w-full h-[1px] bg-slate-400/20 mt-2'></div>
+                <table className='text-center mx-auto mt-2'>
+                  <tr className='text-slate-500'>
+                    <th className='pr-4'>Rank</th>
+                    <th>Player</th>
+                  </tr>
+                  {scoreBoard.map((player, idx) => (
+                    <tr>
+                      <td className='pr-4'>{idx + 1}</td>
+                      <td>{players[player].name}</td>
+                    </tr>
+                  ))}
+                </table>
+              </div>
+            )}
           </>
         ) : null}
         {gameState === 'Finished' && <p>game Finished</p>}
